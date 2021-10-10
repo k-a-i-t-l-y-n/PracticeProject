e29376bef1a1b97fa0889b4fa162e29c5e4757d9
estorey99 <79337310+estorey99@users.noreply.github.com>
2021-10-10 13:53:14 -0500

    Update Program.cs
    I left the AI class blank because of the stuff we talked about yesterday would invalidate some of the functions in it, at least I think.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,73 @@ using System.Threading.Tasks;
 
 namespace PracticeProject
 {
-    class Program
+
+    class UserInput
     {
+        public
+        string title
+        { get; set; }
+        string director { get; set; }
+        string actor { get; set; }
+        int genre { get; set; }
+        string year { get; set; }
     }
-}
+
+    class Movie
+    {
+        public
+        int movieID
+        { get; set; }
+        string title { get; set; }
+        string director { get; set; }
+        string actor { get; set; }
+        int genre { get; set; }
+        string year { get; set; }
+        string description { get; set; }
+    }
+    class ErrorHandling
+    {
+        private
+            bool inputValid;
+        bool listEmpty;
+        public
+            bool noInput(UserInput input)
+        {
+
+        }
+            bool emptyList(List<Movie> movieList)
+            { 
+            }
+            bool invalidInput(UserInput input) 
+            { 
+            }  
+
+    }
+    class MovieList
+    {
+        private
+       List<Movie> movieList;
+        public
+       MovieList()
+        {
+            this.movieList = new List<Movie>();
+        }
+        void addMovie(Movie movie)
+        {
+            this.movieList.Add(movie);
+        }
+        bool removeMovie(int movieID)
+        {
+            return movieList.Remove(getMovie(movieID));
+        }
+        Movie getMovie(int movieId)
+        {
+            return movieList.Find(delegate (Movie movie)
+            {
+                return movie.movieID.Equals(movieId);
+            });
+        }
+        }
+        class AI { }
+
+    }
