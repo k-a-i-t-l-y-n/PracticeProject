ef3d564bc8bca2f9d9986eb2d70c09cc4cefa269
jnrob900 <91620651+jnrob900@users.noreply.github.com>
2021-10-27 11:02:58 -0500

    Add files via upload

---
+++ Window3.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PracticeProject
+{
+    /// <summary>
+    /// Interaction logic for Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+       
+
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        public int Index { get; private set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var win3 = new Window2();
+            win3.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
