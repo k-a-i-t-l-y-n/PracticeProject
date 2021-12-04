ffe7692e1ea375532608cdf4dd3dbaf80e365faa
estorey99 <79337310+estorey99@users.noreply.github.com>
2021-12-04 15:29:36 -0600

    Update JamLibrary.cs

--- JamLibrary/JamLibrary.cs
+++ JamLibrary/JamLibrary.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
-namespace JamLibrary
+namespace PracticeProject
 {
     public class UserInput
     {
