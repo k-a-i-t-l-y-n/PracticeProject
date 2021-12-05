2dc5b79c21470eaa9b2e1409d888947a7ce7fb24
estorey99 <79337310+estorey99@users.noreply.github.com>
2021-12-05 17:35:18 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using JamLibrary;
 namespace PracticeProject
 {
+      public class Backend
+        {
+           public MovieList GetMovieList(UserInput input)
+            {
+                Query query = new Query(input);
+                List<moviesForList> resultList = AI.runAI();
+             
+            resultList = query.SearchQuery(resultList);
+            MovieList movies = new MovieList(resultList);
+            return movies;
 
-}
+            }
+
+        }
+    }
